8ce8ad1da55879c10d8a85a81eeb1be2e1f74279
SouthHealer628 <108107785+wspbh@users.noreply.github.com>
2022-07-23 16:53:48 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #~/../usr/bash
 echo '作者:wspbh github地址:www.github.com/wspbh   gitee地址:www.github.com/sybs'
-echo '我会帮你改为bfsu源'
+echo '推荐改为国内源后再进行安装'
 echo '该脚本需下载近1.7G的文件'
 echo '确保你的网络正常，记得关流量XD'
 echo '否则安装失败不怪我'
@@ -8,19 +8,19 @@ echo '5秒后开始'
 sleep 5
 rm install.sh
 termux-setup-storage
-sed -i 's@^\(deb.*stable main\)$@#\1\ndeb https://mirrors.bfsu.edu.cn/termux/termux-packages-24 stable main@' $PREFIX/etc/apt/sources.list &&apt update && apt upgrade
+apt update && apt upgrade
 cd ~
-pkg install wget curl unzip tar aria2 -y
-echo -e "\033[3xm说起来你可能不信，aria2不配置还没wget快\033[0m"  
+pkg install wget curl unzip tar git -y
+echo -e "\033[3xm说起来你可能不信，aria2不配置还没wget快\033[0m"
 wget https://images.kali.org/nethunter/kalifs-arm64-full.tar.xz
-wget https://hub.fastgit.xyz/wspbh/kali-on-phone/main/install-nethunter-termux
+git clone https://hub.fastgit.xyz/wspbh/kali-on-phone.git
 chmod +x install-nethunter-termux
 ./install-nethunter-termux
 cd ~
 if [ ! -d kali-arm64 ];then
 
 	   echo '安装失败了,可能是你强制停止或网络问题?'
-	   echo '按CTRL+C退出脚本'
+	   echo '按回车退出脚本'
            exit 1
 
 fi
