6facaf989a77a08965ed388fd31cbe671fd8ea6b
SouthHealer628 <108107785+wspbh@users.noreply.github.com>
2022-07-23 17:01:32 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #~/../usr/bash
-echo '作者:wspbh github地址:www.github.com/wspbh   gitee地址:www.github.com/sybs'
+echo '作者:wspbh github地址:www.github.com/wspbh'
 echo '推荐改为国内源后再进行安装'
 echo '该脚本需下载近1.7G的文件'
 echo '确保你的网络正常，记得关流量XD'
@@ -12,8 +12,10 @@ apt update && apt upgrade
 cd ~
 pkg install wget curl unzip tar git -y
 echo -e "\033[3xm说起来你可能不信，aria2不配置还没wget快\033[0m"
-wget https://images.kali.org/nethunter/kalifs-arm64-full.tar.xz
 git clone https://hub.fastgit.xyz/wspbh/kali-on-phone.git
+cd ~
+wget https://images.kali.org/nethunter/kalifs-arm64-full.tar.xz
+mv ~/install-nethunter-termux ~
 chmod +x install-nethunter-termux
 ./install-nethunter-termux
 cd ~
